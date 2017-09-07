5f34b8e465fbba07a231913eeaea14f15f9b2634
gbraide <gbraide@mail.usf.edu>
2017-09-06 22:35:07 -0400

    Convert timestamp to human readable format

--- src/components/Post-item.js
+++ src/components/Post-item.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react'
 class PostItem extends Component {
 	render() {
 		const post = this.props.post
+		const convertTimestamp = new Date(post.timestamp)
+		const date = convertTimestamp.toString()
+
 		return (
 			<section> 
 				<h4>{post.title}</h4>
 				<div>
-					<span><cite>Author: {post.author}</cite></span>
+					<span><cite>Author: {post.author} </cite>|</span>
+					<span><cite><small> {date}</small></cite></span>
 				</div>
 				<hr/>
-				<small>Time: {post.timestamp} |</small>
 				<small> Category: {post.category} |</small>
 				<small> Score: {post.voteScore}</small>
 			</section>
